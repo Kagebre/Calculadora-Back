21a7cf589426797de7c4189189d290d94c2a654a
Benhur <benhur.custodio@gmail.com>
2023-07-03 08:17:41 -0300

    integrar  lista db

--- calculadora_das_trevas.py
+++ calculadora_das_trevas.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-from memoria import sqlite3, registrar_operacao
+from memoria import  registrar_operacao, consultar_operacoes
+
 
 app = Flask(__name__)
 CORS (app)
 
-@app.route('/api/calculo', methods=['GET'])
+@app.route('/calculo', methods=['GET'])
 def calculo():
     
     args = request.args.get('numerosDigitados') 
@@ -17,6 +18,16 @@ def calculo():
     print(str(resultado))
     return str(resultado)
 
-
+@app.route('/ola_mundo', methods=['GET'])
+def ola_mundo():
+    print('olá mundo')
+    return'ok'
+    
+@app.route('/consultar_operacoes', methods=['GET'])
+def cons_op():
+    consultar_operacoes()
+    return'ok'    
+    
 if __name__ == '__main__':
     app.run(port=5003, host='192.168.100.40', threaded=True)
+
